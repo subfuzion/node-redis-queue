13f0efe9e4dc349392f7dc6a87c899e333e3ba78
Tony Pujals <tony@subfuzion.com>
2017-11-06 09:18:34 -0800

    switch to ioredis since it is already promisified

--- test/test.js
+++ test/test.js
@@ -1,16 +1,10 @@
 const assert = require('assert')
 const config = require('config')
-const redis = require('redis')
-const {promisify} = require('util')
+const Redis = require('ioredis')
 
 suite('queue tests', () => {
   // create redis client r with options
   let r, options = {}
-
-  // redis funcs that we want to promisify for async/await
-  let funcs = [ "set", "ping", "rpush", "blpop" ]
-  // object to access promisified redis funcs
-  let rasync = {}
 
   setup(() => {
     // load redis configuration
@@ -18,32 +12,27 @@ suite('queue tests', () => {
     options.host = redisConfig.host
     options.port = redisConfig.port
 
-    r = redis.createClient(options)
-
-    // promisify redis funcs
-    funcs.forEach(func => {
-      rasync[func] = promisify(r[func].bind(r))
-    })
+    r = new Redis(options)
   })
 
   teardown(() => {
-    // make sure to quit the redis client or mocha won't exit
+    // make sure to tell redis to close connections or mocha won't exit
     r.quit()
   })
 
   suite('basic redis tests', () => {
     test('ping redis', async () => {
-      let res = await rasync.ping()
+      let res = await r.ping()
       assert.equal(res, "PONG")
     })
 
     test('retrieve messages from redis queue in fifo order', async () => {
       let key = "queue"
       let vals = [ "a", "b", "c" ]
-      await rasync.rpush(key, ...vals)
+      await r.rpush(key, ...vals)
 
       vals.forEach(async v => {
-        let result = await rasync.blpop(key, 1)
+        let result = await r.blpop(key, 1)
         // result is an array [ list, value ], e.g., [ "queue","a" ]
         // console.log(`${result[1]} should equal ${v}`)
         assert.equal(result[1], v)
@@ -55,6 +44,7 @@ suite('queue tests', () => {
   suite('producer-consumer tests', () => {
 
     test('send and receive messages using producer and consumer', async () => {
+
     })
 
   }) // producer-consumer tests
