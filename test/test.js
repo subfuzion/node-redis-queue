f096fa21f77f8988dacbc6842a454cdc1a5680e0
Tony Pujals <tony@subfuzion.com>
2017-11-07 18:10:59 -0800

    basic producer consumer support implemented

--- test/test.js
+++ test/test.js
@@ -1,23 +1,25 @@
 const assert = require('assert')
-const config = require('config')
+const Consumer = require('../Consumer')
+const Producer = require('../Producer')
+const QueueBase = require('../QueueBase')
 const Redis = require('ioredis')
+
+const topic = 'queue'
 
 suite('queue tests', () => {
   // create redis client r with options
-  let r, options = {}
+  let r, options = {
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 6379
+  }
 
   setup(() => {
-    // load redis configuration
-    let redisConfig = config.get('redis')
-    options.host = redisConfig.host
-    options.port = redisConfig.port
-
     r = new Redis(options)
   })
 
-  teardown(() => {
+  teardown(async () => {
     // make sure to tell redis to close connections or mocha won't exit
-    r.quit()
+    await r.quit()
   })
 
   suite('basic redis tests', () => {
@@ -27,13 +29,14 @@ suite('queue tests', () => {
     })
 
     test('retrieve messages from redis queue in fifo order', async () => {
-      let key = "queue"
+      let key = topic
       let vals = [ "a", "b", "c" ]
       await r.rpush(key, ...vals)
 
+      // check results received in same order sent
       vals.forEach(async v => {
         let result = await r.blpop(key, 1)
-        // result is an array [ list, value ], e.g., [ "queue","a" ]
+        // result is an array [ list, value ], e.g., [ "queue", "a" ]
         // console.log(`${result[1]} should equal ${v}`)
         assert.equal(result[1], v)
       })
@@ -43,8 +46,54 @@ suite('queue tests', () => {
 
   suite('producer-consumer tests', () => {
 
-    test('send and receive messages using producer and consumer', async () => {
+    test('new connection successfully pings', async () => {
+      let conn = new QueueBase(topic)
+      let res = await conn.ping()
+      assert.equal(res, "PONG")
+      await conn.quit()
+    })
 
+    test('using connection after quit throws an error', async () => {
+      let conn = new QueueBase(topic)
+      await conn.quit()
+      try {
+        await conn.ping()
+        throw new Error('expected ping after quit to fail')
+      } catch (err) {
+        let expected = 'Connection is closed.'
+        assert.equal(err.message, expected)
+      }
+    })
+
+    test('send and receive messages using producer and consumer', async () => {
+      let p = new Producer(topic)
+      let c = new Consumer(topic)
+      let vals = [ "a", "b", "c" ]
+      vals.forEach(async v => {
+        await p.send(v)
+      })
+
+      // check results received in same order sent
+      vals.forEach(async v => {
+        let result = await c.receive()
+        assert.equal(result, v)
+      })
+
+      await p.quit()
+      await c.quit()
+    })
+
+    test('can quit waiting consumer', async () => {
+      let c = new Consumer(topic)
+
+      // wait 200 ms and then close the connection
+      setTimeout(() => {
+        c.quit()
+      }, 200)
+
+      // blocking wait should return with null as soon as conn is closed when timer fires
+      let result = await c.receive()
+      assert.equal(result, null)
     })
 
   }) // producer-consumer tests
